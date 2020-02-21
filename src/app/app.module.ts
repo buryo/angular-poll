4eba640d984e39aa874d9c484376950ebee497be
Burak <burak.sen@conclusion.nl>
2020-02-21 16:25:43 +0100

    Add new layout.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,14 @@ import { AppComponent } from './app.component';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { environment } from '../environments/environment';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { Error404PageComponent } from './pages/error404-page/error404-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    Error404PageComponent
   ],
   imports: [
     BrowserModule,
