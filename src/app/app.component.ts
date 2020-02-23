4c5b1e9fc94dd5de2d02640dfebc9f085e9e3458
Burak <burak.sen@conclusion.nl>
2020-02-23 22:15:23 +0100

    Simplified app component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,10 @@
 import { Component } from '@angular/core';
-import { AngularFirestore } from '@angular/fire/firestore';
-import { Observable } from 'rxjs';
 import 'firebase/firestore';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  polls: Observable<any[]>;
-  constructor(firestore: AngularFirestore) {
-    this.polls = firestore.collection('poll').valueChanges();
-  }
+
 }
