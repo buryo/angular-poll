fe6b01f15da712c6a739a7240da5b726fa0435d8
Burak <burak.sen@conclusion.nl>
2020-02-23 22:18:47 +0100

    Add routes.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,29 @@
+import { PollViewComponent } from './modules/polls/poll-view/poll-view.component';
+import { Error404PageComponent } from './pages/error404-page/error404-page.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomePageComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: 'poll/:id',
+    component: PollViewComponent,
+  },
+  {
+    path: '404',
+    component: Error404PageComponent,
+  },
+  {
+    path: '**',
+    redirectTo: '/404'
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
