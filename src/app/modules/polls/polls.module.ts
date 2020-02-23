9ac82ef233fe0cd46ba2859da513c9e5286c066e
Burak <burak.sen@conclusion.nl>
2020-02-23 22:20:11 +0100

    😙

---
+++ src/app/modules/polls/polls.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { PollViewComponent } from './poll-view/poll-view.component';
+
+
+
+@NgModule({
+  declarations: [PollViewComponent],
+  imports: [
+  ]
+})
+export class PollsModule { }
