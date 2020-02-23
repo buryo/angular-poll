c931bf88579386df5365fa30b67cdec868a5ff56
Burak <burak.sen@conclusion.nl>
2020-02-23 22:17:03 +0100

    Home page rendering all polls in poll-card comp.

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,15 +1,26 @@
+import { Poll } from './../../modules/polls/shared/poll/poll.model';
+import { PollService } from './../../modules/polls/shared/poll/poll.service';
+import { Observable } from 'rxjs';
 import { Component, OnInit } from '@angular/core';
+import { map, defaultIfEmpty } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
-  styleUrls: ['./home-page.component.scss']
+  styleUrls: ['./home-page.component.scss'],
 })
 export class HomePageComponent implements OnInit {
+  polls: Observable<Poll[]>;
 
-  constructor() { }
+  constructor(private PollService: PollService) {
+
+  }
 
   ngOnInit(): void {
+    this.polls = this.PollService.getPolls().pipe(
+      map(polls => polls.slice(0, 10)),
+      defaultIfEmpty([])
+    );
   }
 
 }
